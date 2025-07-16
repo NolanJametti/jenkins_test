7c1548c9fa3c602ecd2fff7d4b28783ba786f8ef
NolanJametti <nolan.jametti@gmail.com>
2025-07-16 20:33:13 +0200

    🔖 v2.0.0

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 let win;
 let tray;
+let isInteractiveMode = false;
 
 // Gestion des erreurs globales
 process.on('uncaughtException', (error) => console.error('Uncaught Exception:', error));
@@ -40,10 +41,7 @@ function createWindow() {
     .catch(err => console.error('Erreur loadFile:', err));
 }
 
-function createTray() {
-  const iconPath = path.join(__dirname, 'icons', 'tray-icon.png');
-  tray = new Tray(iconPath);
-
+function updateTrayMenu() {
   const contextMenu = Menu.buildFromTemplate([
     {
       label: "Afficher/masquer l'animation",
@@ -52,11 +50,27 @@ function createTray() {
       }
     },
     {
-      label: 'Appeler Spike',
+      label: 'Spike !',
       click: () => {
-        // Envoie un message au renderer pour déclencher l'animation
         if (win && !win.isDestroyed()) {
           win.webContents.send('call-spike');
+        }
+      }
+    },
+    {
+      label: isInteractiveMode ? 'Oust !' : 'Viens ici',
+      click: () => {
+        if (win && !win.isDestroyed()) {
+          if (isInteractiveMode) {
+            // Envoie le message pour arrêter le mode interactif
+            win.webContents.send('dismiss-spike');
+            isInteractiveMode = false;
+          } else {
+            // Envoie le message pour démarrer le mode interactif
+            win.webContents.send('come-here');
+            isInteractiveMode = true;
+          }
+          updateTrayMenu(); // Met à jour le menu après le changement
         }
       }
     },
@@ -68,8 +82,14 @@ function createTray() {
     }
   ]);
 
-  tray.setToolTip('Skipe');
   tray.setContextMenu(contextMenu);
+}
+
+function createTray() {
+  const iconPath = path.join(__dirname, 'icons', 'tray-icon.png');
+  tray = new Tray(iconPath);
+  tray.setToolTip('Spike');
+  updateTrayMenu();
 }
 
 app.whenReady().then(() => {
--- preload.js
+++ preload.js
@@ -1,5 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  onCallSpike: (callback) => ipcRenderer.on('call-spike', callback)
+  onCallSpike: (callback) => ipcRenderer.on('call-spike', callback),
+  onComeHere: (callback) => ipcRenderer.on('come-here', callback),
+  onDismissSpike: (callback) => ipcRenderer.on('dismiss-spike', callback)
 });
